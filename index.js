3e41a958a780a7d03061c5b0cc05309cdc748b03
Ghilain <ishimweghilain5050@gmail.com>
2022-04-26 01:48:24 +0200

    fix linter errors

--- index.js
+++ index.js
@@ -1,27 +1,70 @@
+// Create Dom element
 
+const addBooks = localStorage.getItem('adbooks');
+let books = [];
 
+if (addBooks) {
+  books = JSON.parse(addBooks);
+} else {
+  const bookData = [
+    {
+      id: 1,
+      name: 'Lorem ipsum',
+      author: 'Testeroo Testyy',
+    },
+  ];
 
-const bookinput=document.getElementById('bookinput');
-const displaybook=document.getElementById('displaybook');
-const title=bookinput['title1'];
-const author=bookinput['author1'];
+  localStorage.setItem('adbooks', JSON.stringify(bookData));
+  books = JSON.parse(addBooks);
+  window.location.reload();
+}
+// Display book information
 
-const awesomebook=[{
-    title:'mr',
-    author:'david',
-}];
+const AddBook = document.querySelector('#displaybook');
+books.forEach((book) => {
+  const newBook = `
+  <p>${book.name}</p>
+  <p>${book.author}</p>
+  <button onclick=deleteBook(${book.id}) class="removebtn"> Remove</button>
+  <hr>
+  `;
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('test');
+  bookDiv.innerHTML = newBook;
+  AddBook.appendChild(bookDiv);
+});
+// Remove button function
 
-const addBook =(title1,author1)=>{};
-const createBookElement=()=>{
-const bookdiv=document.createElement('div');
-const booktitle=document.createElement('p');
-const bookauthor=document.createElement('p');
+function deleteBook(id) {
+  if (localStorage.getItem('adbooks') === null) {
+    books = books.filter((book) => {
+      if (id === book.id) {
+        return false;
+      }
+      return true;
+    });
+    localStorage.setItem('adbooks', JSON.stringify(books));
+    window.location.reload();
+  } else {
+    localStorage.removeItem('adbooks', JSON.stringify(books));
+  }
+}
+document.addEventListener('DOMContentLoaded', deleteBook);
+// Add book information
 
-booktitle.innerText=" book title "+title1;
-bookauthor.innerText=" book author "+author1;
-
-bookdiv.append(booktitle,bookauthor);  
-displaybook.appendChild(bookdiv); 
-};
-
-awesomebook.forEach(createBookElement);
+const addBtn = document.querySelector('#add');
+addBtn.addEventListener('click', () => {
+  const name = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  let id = 0;
+  if (books.length > 0) {
+    id = books[books.length - 1].id + 1;
+  }
+  books.push({
+    id,
+    name,
+    author,
+  });
+  localStorage.setItem('adbooks', JSON.stringify(books));
+  window.location.reload();
+});
